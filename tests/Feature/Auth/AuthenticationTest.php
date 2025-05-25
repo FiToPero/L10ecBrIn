75f627ae296301b61d450a4aa8634eb5a803a0ae
FitoPero <fito.pero@gmail.com>
2025-05-25 18:09:02 -0300

    proof 10

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -31,7 +31,7 @@ class AuthenticationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(Route('product.index'));
+        $response->assertRedirect(route('product.index'));
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
