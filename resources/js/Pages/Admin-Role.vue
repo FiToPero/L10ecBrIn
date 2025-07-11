7b57513a3faef64927e3de851380b346859b6636
FitoPero <fito.pero@gmail.com>
2025-07-11 19:52:59 -0300

    fix roles

--- resources/js/Pages/Admin-Role.vue
+++ resources/js/Pages/Admin-Role.vue
@@ -42,6 +42,7 @@ const saveRole = (save) => {
     router.post(route('adminRole.update'), save)
     modalView.value = false
     responseSave = ''
+    viewSave.value = []
 }
 const cancelUpdate = (cancel) => {    
     const rolesOriginal = JSON.parse(JSON.stringify(props.roles))
@@ -153,15 +154,7 @@ watch(() => page.props.flash.message, (newValue) => {
                                 </td>
                                 <td id="badge" class="  flex flex-wrap gap-1 p-2 font-medium text-gray-900 whitespace-nowrap">
                                     <template v-for="permission in role.permissions" :key="'permissions'+permission.id">
-                                        
-<span class="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-blue-800 bg-blue-100 rounded-sm dark:bg-blue-900 dark:text-blue-300">
-    {{ permission.name }}
-    <button @click="removeBadge(permission.id, index)" class="inline-flex items-center p-1 ms-2 text-sm text-blue-400 bg-transparent rounded-xs hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300" >
-        <svg class="w-2 h-2" aria-hidden="tru</button>e" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        </svg>
-    </button>
-</span>
+                                        <Badge :infoText="permission.name" :infoId="permission.id" :index="index" @removeBadge="removeBadge" />
                                     </template>
                                 </td>
                                 <td class="p-5 font-medium" >
