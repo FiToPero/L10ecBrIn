e0674297916b6fca2c44f35ba8e9d80ba0a76cb3
FitoPero <fito.pero@gmail.com>
2025-08-20 00:29:57 -0300

    fix and I improve roles-admin

--- resources/js/Pages/Admin-Role.vue
+++ resources/js/Pages/Admin-Role.vue
@@ -9,70 +9,79 @@ import Create from '@/Pages/Forms/RolesForms/Create.vue'
 import Modal from '@/Components/Modal.vue'
 
 const props = defineProps({
-    roles: { type: Object, required: true },
-    permissions: { type: Object, required: true },
+    roles: { type: Array, required: true },
+    permissions: { type: Array, required: true },
 })
 const page = usePage()
 
+const rolesOriginal = props.roles
+const rolesTemp = ref(JSON.parse(JSON.stringify(props.roles)))
 const isMessage = ref(false)
 const modalCreate = ref(false)
-const rolesOld = ref(props.roles)
 const modalView = ref(false)
 const viewSave = ref([])
 const addPermission = ref('')
 let responseSave = ''
 
 const openCreate = () => { modalCreate.value = !modalCreate.value }
-const openModalView = (role) => { 
+
+//////// Functions  Permission ///////////
+const removePermission = (perId, roleId, index) => {  
+    rolesTemp.value = rolesTemp.value.map(role => {
+        if (role.id === roleId) {
+            role.permissions = role.permissions.filter(p => p.id !== perId)
+        }
+        return role
+    })
+    markAsModified(index, false)
+}
+watch(addPermission, (newValue) => {
+    rolesTemp.value = rolesTemp.value.map(role => {
+        if (role.id === newValue.roleId) {
+            const exists = role.permissions.some(p => Number(p.id) === Number(newValue.id))
+            if (!exists) {
+                role.permissions.push({id: newValue.id, name: newValue.name})
+            }
+        }
+        return role
+    })
+    markAsModified(newValue.index, false)
+    addPermission.value = ''
+})
+//////////////////////////////////////////////
+
+const markAsModified = (index, remove) => {
+    if (remove) {
+        viewSave.value = viewSave.value.filter(i => i !== index)
+    }else{
+        if (!viewSave.value.includes(index)) {
+            viewSave.value.push(index)
+        }
+    }
+}
+
+//////////////// Save Role /////////////////
+const openModalView = (role, index) => { 
     modalView.value = !modalView.value
     responseSave =  JSON.parse(JSON.stringify(role))
+    cancelUpdate(index, role.id)
 }
 
-//////// Functions ///////////
-const removeBadge = (id, index) => {  
-    const rolesCopy = JSON.parse(JSON.stringify(rolesOld.value)) // Clona todo el array rolesOld para asegurar reactividad y evitar proxies
-    rolesCopy[index].permissions = rolesCopy[index].permissions.filter(p => Number(p.id) !== Number(id)) 
-    rolesOld.value = rolesCopy // Reasigna el array completo
-
-    if(!viewSave.value.some(item => item === index)){
-        viewSave.value.push(index)
-    } 
-}
 const saveRole = (save) => {
     router.post(route('adminRole.update'), save)
     modalView.value = false
     responseSave = ''
-    viewSave.value = []
 }
-const cancelUpdate = (cancel) => {    
-    const rolesOriginal = JSON.parse(JSON.stringify(props.roles))
-    let rolePermissionsOriginal = rolesOriginal[cancel] 
-
-    const viewSaveCopy = JSON.parse(JSON.stringify(viewSave.value))
-    if(viewSaveCopy.filter(i => i !== cancel)){
-        viewSave.value = viewSaveCopy.filter(i => i !== cancel)
-        rolesOld.value[cancel] = rolePermissionsOriginal
-    }
+//////////////////////////////////////
+const cancelUpdate = (index, roleId) => {    
+    rolesTemp.value = rolesTemp.value.map(role => {
+        if (role.id === roleId) {
+            Object.assign(role, JSON.parse(JSON.stringify(rolesOriginal.find(roleOriginal => roleOriginal.id === roleId))))
+        }
+        return role
+    })
+    markAsModified(index, true)
 }
-
-watch(addPermission, (newValue) => {
-    const rolesCopy = JSON.parse(JSON.stringify(rolesOld.value))
-    const exists = rolesCopy[newValue.index].permissions.some(p => Number(p.id) === Number(newValue.id))
-    if (!exists) {
-        rolesCopy[newValue.index].permissions.push({id: newValue.id, name: newValue.name})
-    }
-    rolesOld.value = rolesCopy
-
-    if(!viewSave.value.some(item => item === newValue.index)){
-        viewSave.value.push(newValue.index)
-    }
-})
-watch(
-  () => props.roles,
-  (newRoles) => {
-    rolesOld.value = JSON.parse(JSON.stringify(newRoles))
-  }
-)
 
 ///////  Mesage Flash ///////
 const closeMessage = () => { 
@@ -112,7 +121,6 @@ watch(() => page.props.flash.message, (newValue) => {
                 <ButtonColor text="white" bg="cyan"  @click=saveRole(responseSave)>{{ $t('Update Role') }}</ButtonColor>
             </div>
         </Modal>
-
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -136,8 +144,8 @@ watch(() => page.props.flash.message, (newValue) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <template v-for="(role, index) in rolesOld" :key="'roles'+index">
-                            <tr class="    bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <template v-for="(role, index) in rolesTemp" :key="'roles'+index">
+                            <tr class=" bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <th scope="row" class="p-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     <p class="dark:text-white">{{ role.id }}</p>
                                 </th>
@@ -147,20 +155,21 @@ watch(() => page.props.flash.message, (newValue) => {
                                 <td class="p-2 m-10 font-medium text-gray-900 whitespace-nowrap">
                                     <label class='block m-2 text-sm text-gray-900 dark:text-white font-bold'>{{ $t('Select Permission') }}</label>
                                     <select v-model="addPermission" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                        <template v-for="permission in permissions" :key="'select'+role.id" >
-                                            <option :value="{id: permission.id, name: permission.name, index}"   >{{ permission.name }}</option>
+                                         <option value="">{{ $t('Select...') }}</option>
+                                        <template v-for="permission in permissions" :key="'select'+role.id">
+                                            <option v-if="!role.permissions.some(p => p.id === permission.id)" :value="{id: permission.id, name: permission.name, index, roleId: role.id}" >{{ permission.name }} </option>
                                         </template>
                                     </select>   
                                 </td>
                                 <td id="badge" class="  flex flex-wrap gap-1 p-2 font-medium text-gray-900 whitespace-nowrap">
                                     <template v-for="permission in role.permissions" :key="'permissions'+permission.id">
-                                        <Badge :infoText="permission.name" :infoId="permission.id" :index="index" @removeBadge="removeBadge" />
+                                        <Badge :text="permission.name" :perId="permission.id" :roleId="role.id" :index="index" @remove="removePermission" />
                                     </template>
                                 </td>
                                 <td class="p-5 font-medium" >
                                     <div class="flex justify-center gap-2">
-                                    <ButtonColor text="white" bg="green" v-if="page.props.auth.user.permissions.includes('update_role') && viewSave.some(item => item === index)" @click="openModalView(role)"  >{{ $t('Save') }}</ButtonColor>
-                                    <ButtonColor text="white" bg="red" v-if="page.props.auth.user.permissions.includes('update_role') && viewSave.some(item => item === index)" @click="cancelUpdate(index)"  >{{ $t('Cancel') }}</ButtonColor>
+                                    <ButtonColor text="white" bg="green" v-if="page.props.auth.user.permissions.includes('update_role') && viewSave.some(item => item === index)" @click="openModalView(role, index)"  >{{ $t('Save') }}</ButtonColor>
+                                    <ButtonColor text="white" bg="red" v-if="page.props.auth.user.permissions.includes('update_role') && viewSave.some(item => item === index)" @click="cancelUpdate(index, role.id)"  >{{ $t('Cancel') }}</ButtonColor>
                                     </div>
                                 </td>
                             </tr>
